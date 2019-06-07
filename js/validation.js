f61137b7523423e74f19c00aba331c8515efe8c9
Ana Beatriz Santiago <ana.santiago@redeinova.com>
2019-06-07 16:13:26 -0300

    Validação com regex

--- js/validation.js
+++ js/validation.js
@@ -13,12 +13,12 @@ function validarCadastro() {
     let confirmaSenha = document.getElementById('confirmasenha');
 
     msgNome = document.querySelector('.erro-nome');
-    if(nome.value.length < 3) {
+    if(nome.value.length == '') {
         msgNome.innerHTML = "Por favor, preencha o campo Nome!";
         msgNome.style.opacity = 1;
         msgNome.style.transition = '1s';
         nome.style.border = 'solid 1px red';
-        nome.onkeypress = digitar => {
+        nome.onkeypress = () => {
             nome.style.border = 'solid 1px green';
             msgNome.style.opacity = 0;
         }
@@ -27,46 +27,36 @@ function validarCadastro() {
         msgNome.style.opacity = 0;
     }
 
+    const regexCpf = /[\d]{3}.[\d]{3}.[\d]{3}-[\d]{2}/;
     msgCpf = document.querySelector('.erro-cpf');
-    if(cpf.value.length != 11) {
+    if(!regexCpf.test(cpf.value)) {
         msgCpf.innerHTML = "Por favor, informe um Cpf válido!";
         msgCpf.style.opacity = 1;
         msgCpf.style.transition = '1s';
         cpf.style.border = 'solid 1px red';
-        cpf.onkeypress = digitar => {
-            cpf.style.border = 'solid 1px green';
-            msgCpf.style.opacity = 0;
-        }
     } else {
         cpf.style.border = 'solid 1px green';
         msgCpf.style.opacity = 0;
     }
 
     msgData = document.querySelector('.erro-data');
-    if(data.value.length != 10) {
+    if(!data.value) {
         msgData.innerHTML = "Por favor, informe uma Data válida!";
         msgData.style.opacity = 1;
         msgData.style.transition = '1s';
         data.style.border = 'solid 1px red';
-        data.onkeypress = digitar => {
-            data.style.border = 'solid 1px green';
-            msgData.style.opacity = 0;
-        }
     } else {
         data.style.border = 'solid 1px green';
         msgData.style.opacity = 0;
     }
 
+    const regexTelefone = /\([\d]{2}\)[\d]{5}-[\d]{4}/;
     msgTelefone = document.querySelector('.erro-telefone');
-    if(telefone.value.length < 11 ) {
+    if(!regexTelefone.test(telefone.value)) {
         msgTelefone.innerHTML = "Por favor, informe um Telefone válido!";
         msgTelefone.style.opacity = 1;
         msgTelefone.style.transition = '1s';
         telefone.style.border = 'solid 1px red';
-        telefone.onkeypress = digitar => {
-            telefone.style.border = 'solid 1px green';
-            msgTelefone.style.opacity = 0;
-        }
     } else {
         telefone.style.border = 'solid 1px green';
         msgTelefone.style.opacity = 0;
@@ -82,37 +72,30 @@ function validarCadastro() {
     }
 
     msgEndereco = document.querySelector('.erro-endereco');
-    if(endereco.value.length < 3 ) {
+    if(endereco.value.length == '') {
         msgEndereco.innerHTML = "Por favor, informe seu endereço.";
         msgEndereco.style.opacity = 1;
         msgEndereco.style.transition = '1s';
         endereco.style.border = 'solid 1px red';
-        endereco.onkeypress = digitar => {
-            endereco.style.border = 'solid 1px green';
-            msgEndereco.style.opacity = 0;
-        }
     } else {
         endereco.style.border = 'solid 1px green';
         msgEndereco.style.opacity = 0;
     }
 
+    const regexCep = /[\d]{5}-[\d]{3}/;
     msgCep = document.querySelector('.erro-cep');
-    if(cep.value.length < 3 ) {
+    if(!regexCep.test(cep.value)) {
         msgCep.innerHTML = "Por favor, informe um Cep válido!";
         msgCep.style.opacity = 1;
         msgCep.style.transition = '1s';
         cep.style.border = 'solid 1px red';
-        cep.onkeypress = digitar => {
-            cep.style.border = 'solid 1px green';
-            msgCep.style.opacity = 0;
-        }
     } else {
         cep.style.border = 'solid 1px green';
         msgCep.style.opacity = 0;
     }
 
     msgLogradouro = document.querySelector('.erro-logradouro');
-    if(logradouro.value == '' ) {
+    if(!logradouro.value) {
         msgLogradouro.innerHTML = "Por favor, selecione seu logradouro.";
         msgLogradouro.style.opacity = 1;
         msgLogradouro.style.transition = '1s';
@@ -120,16 +103,13 @@ function validarCadastro() {
         msgLogradouro.style.opacity = 0;
     }
 
+    const regexEmail = /[A-Za-z0-9]+@[a-z]+\.[a-z]{3}/;
     msgEmail = document.querySelector('.erro-email');
-    if(email.value.length < 3 ) {
+    if(!regexEmail.test(email.value)) {
         msgEmail.innerHTML = "Por favor, informe um E-mail válido!";
         msgEmail.style.opacity = 1;
         msgEmail.style.transition = '1s';
         email.style.border = 'solid 1px red';
-        email.onkeypress = digitar => {
-            email.style.border = 'solid 1px green';
-            msgEmail.style.opacity = 0;
-        }
     } else {
         email.style.border = 'solid 1px green';
         msgEmail.style.opacity = 0;
@@ -141,25 +121,17 @@ function validarCadastro() {
         msgSenha.style.opacity = 1;
         msgSenha.style.transition = '1s';
         senha.style.border = 'solid 1px red';
-        senha.onkeypress = digitar => {
-            senha.style.border = 'solid 1px green';
-            msgSenha.style.opacity = 0;
-        }
     } else {
         senha.style.border = 'solid 1px green';
         msgSenha.style.opacity = 0;
     }
 
     msgConfirmaSenha = document.querySelector('.erro-confirma-senha');
-    if(senha.value.length < 5 || senha.value.length > 10 ) {
+    if(confirmaSenha.value != senha.value || confirmaSenha.value == '' || confirmaSenha.value.length < 5 || confirmaSenha.value.length > 10 ) {
         msgConfirmaSenha.innerHTML = "Confirmação de senha incorreto.";
         msgConfirmaSenha.style.opacity = 1;
         msgConfirmaSenha.style.transition = '1s';
         confirmaSenha.style.border = 'solid 1px red';
-        confirmaSenha.onkeypress = digitar => {
-            confirmaSenha.style.border = 'solid 1px green';
-            msgConfirmaSenha.style.opacity = 0;
-        }
     } else {
         confirmaSenha.style.border = 'solid 1px green';
         msgConfirmaSenha.style.opacity = 0;
